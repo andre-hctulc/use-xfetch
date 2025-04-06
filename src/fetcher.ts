94866b3fda0d7d372823d2b20e37d65f29d9f1de
andre-hctulc <andre.tho.44@gmail.com>
2025-04-06 07:18:03 +0200

    >>

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,6 +1,6 @@
 import { xfetch } from "@edgeshiftlabs/xfetch";
 import { mergeRequestInit as mergeParams, replacePathVariables } from "./helpers.js";
-import { RequestInitPart, XCacheKey } from "./types.js";
+import { FetcherArgs, RequestInitPart, XCacheKey } from "./types.js";
 
 /**
  * Latter request inits take precedence over the former ones.
@@ -11,10 +11,12 @@ export function createFetcher(
 ): { fetcher: (args: XCacheKey) => Promise<any>; key: XCacheKey } {
     const staticArgs = mergeParams(...staticParams);
 
-    const fetcher = (args: XCacheKey) => {
+    // `key` - given in `useSWR`, `useInfinite and, `useSWRMutation` context
+    // `arg` - given in `useSWRMutation` context
+    const fetcher = (key: XCacheKey, { arg }: { arg?: FetcherArgs } = {}) => {
         // We cannot mutate the url inside fetcher,
         // // because the fetch url would mismatch the url in the swr key
-        const dynamicArgs = mergeParams(staticArgs, args);
+        const dynamicArgs = mergeParams(staticArgs, key, arg || {});
         const url = replacePathVariables(urlLike, dynamicArgs.pathVariables || {});
         return xfetch(url, dynamicArgs);
     };
